c19d6cc6cfc270067cb61789ce19169387a9660f
Rishi40 <rishipandya44@gmail.com>
2020-11-01 18:57:30 +0530

    Initialization

---
+++ src/util/MyPopup.js
@@ -0,0 +1,15 @@
+import {Popup} from 'semantic-ui-react';
+import React from 'react';
+
+function MyPopup({content,children}) {
+    return (
+        <Popup 
+            inverted
+            content={content}
+            trigger={children}
+        />
+
+    )
+}
+
+export default MyPopup;
